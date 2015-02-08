1aaaa12a6973a742cf44600cf164863316a58261
michael corbridge <mcorbridge@gmail.com>
2015-02-08 10:11:32 -0500

    finally solved the thumb swipe!

--- app/src/main/java/com/mcorbridge/passwordprotector/MainActivity.java
+++ app/src/main/java/com/mcorbridge/passwordprotector/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mcorbridge.passwordprotector;
 
+import android.animation.ValueAnimator;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -60,23 +61,23 @@ public class MainActivity extends BaseActivity {
 
         seekBar = (SeekBar)findViewById(R.id.myseek);
         seekBar.setProgress(100);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                if(progress < 30){
-                    doNext();
+                if(seekBar.getProgress() == 0){
+                  doNext();
                 }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                //stub
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                doThumbAnimation();
             }
         });
 
@@ -87,13 +88,28 @@ public class MainActivity extends BaseActivity {
                     .setIcon(R.drawable.alert_icon)
                     .setPositiveButton("Yikes!", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-// stub
+                            // stub
                         }
                     })
                     .show();
         }
 
         applicationModel.setTimeoutAware(false);
+    }
+
+    private void doThumbAnimation(){
+        ValueAnimator anim = ValueAnimator.ofInt(seekBar.getProgress(), 0);
+        anim.setDuration(100);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int animProgress = (Integer) animation.getAnimatedValue();
+                seekBar.setProgress(animProgress);
+            }
+        });
+
+        anim.start();
     }
 
     @Override
