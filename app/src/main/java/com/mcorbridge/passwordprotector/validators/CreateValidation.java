599300c69cac4454be14d47da75ae9b0bcf9d599
michael corbridge <mcorbridge@gmail.com>
2015-02-09 14:09:19 -0500

    form validation update

--- app/src/main/java/com/mcorbridge/passwordprotector/validators/CreateValidation.java
+++ app/src/main/java/com/mcorbridge/passwordprotector/validators/CreateValidation.java
@@ -9,6 +9,8 @@ import com.mcorbridge.passwordprotector.R;
 /**
  * Created by Mike on 2/9/2015.
  * copyright Michael D. Corbridge
+ *
+ * String validation removes all whitespaces and non visible characters such as tab
  */
 public class CreateValidation {
 
@@ -16,10 +18,16 @@ public class CreateValidation {
     private static String category;
     private static String title;
     private static String value;
-    private static String errorMsg;
 
+    /**
+     *
+     * @param cntxt
+     * @param ctgry
+     * @param ttl
+     * @param vl
+     * @return
+     */
     public static boolean validate(Context cntxt, String ctgry, String ttl, String vl){
-        errorMsg = "";
         context = cntxt;
         category = ctgry;
         title = ttl;
@@ -35,16 +43,21 @@ public class CreateValidation {
             msg += valueError();
         }
         if(msg.length() != 0){
-            errorMsg = msg;
-            errorDialog();
+            errorDialog(msg);
             return false;
         }else{
             return true;
         }
     }
 
+    /**
+     *
+     * @param cntxt
+     * @param ttl
+     * @param vl
+     * @return
+     */
     public static boolean validate(Context cntxt, String ttl, String vl){
-        errorMsg = "";
         context = cntxt;
         title = ttl;
         value = vl;
@@ -56,46 +69,72 @@ public class CreateValidation {
             msg += valueError();
         }
         if(msg.length() != 0){
-            errorMsg = msg;
-            errorDialog();
+            errorDialog(msg);
             return false;
         }else{
             return true;
         }
     }
 
+    /**
+     *
+     * @return
+     */
     public static boolean categoryValidation(){
-        return  (category.length() == 0);
+        return  (category.replaceAll("\\s+","").length() == 0);
     }
 
+    /**
+     *
+     * @return
+     */
     public static boolean titleValidation(){
-        return  (title.length() == 0);
+        return  (title.replaceAll("\\s+","").length() == 0);
     }
 
+    /**
+     *
+     * @return
+     */
     public static boolean valueValidation(){
-        return  (value.length() == 0);
+        return  (value.replaceAll("\\s+","").length() == 0);
     }
 
+    /**
+     *
+     * @return
+     */
     private static String categoryError(){
-        return "The category cannot be left empty\n";
+        return "The Category cannot be left empty\n";
     }
 
+    /**
+     *
+     * @return
+     */
     private static String titleError(){
-        return "The title cannot be left empty\n";
+        return "The Title cannot be left empty\n";
     }
 
+    /**
+     *
+     * @return
+     */
     private static String valueError(){
-        return "The value cannot be left empty";
+        return "The Value cannot be left empty";
     }
 
-    public static void errorDialog(){
+    /**
+     *
+     */
+    public static void errorDialog(String errorMsg){
          new AlertDialog.Builder(context)
                 .setTitle("Alert")
                 .setMessage("The following errors were found:\n\n" + errorMsg)
                 .setIcon(R.drawable.alert_icon)
                 .setPositiveButton("Ok, got it", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        //startActivity(intent);
+                        //stub
                     }
                 }).show();
     }
